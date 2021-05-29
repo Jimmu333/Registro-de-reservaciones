6a70625fd2bea7afe7d67eca58f7371efe480a86
odal1is <orodriguezr7@miumg.edu.gt>
2021-05-28 18:14:16 -0600

    Update empleados.cpp
    es lo unico que pude hace :')

--- empleados.cpp
+++ empleados.cpp
@@ -7,14 +7,6 @@
  
 using namespace std;
 
-void gotoxy(int x,int y){  
-      HANDLE hcon;  
-      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
-      COORD dwPos;  
-      dwPos.X = x;  
-      dwPos.Y= y;  
-      SetConsoleCursorPosition(hcon,dwPos);  
- }
  
  void guardar(),mostrar(),buscar(),modificar(),eliminar(),ver();
 
